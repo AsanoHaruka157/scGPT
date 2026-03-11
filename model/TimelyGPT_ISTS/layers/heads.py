5857e3faa3f4fb69c045f81f5978c8ec5da3caee
AsanoHaruka157 <chxi780@gmail.com>
2026-03-11 10:27:33 -0400

    Clean up history and restore repository

---
+++ model/TimelyGPT_ISTS/layers/heads.py
@@ -0,0 +1,61 @@
+<<<<<<< HEAD
+import torch.nn as nn
+from layers.snippets import SigmoidRange
+
+class PretrainHead(nn.Module):
+    def __init__(self, d_model, n_output):
+        super(PretrainHead, self).__init__()
+        self.head = nn.Linear(d_model, n_output + 1)  # +1 to include the padding token as a class
+
+    def forward(self, x):
+        """
+        x: tensor [batch_size x seq_len x d_model]
+        output: tensor [batch_size x seq_len x n_output]
+        """
+        return self.head(x)
+
+
+class ClfHead(nn.Module):
+    def __init__(self, d_model, n_output):
+        super(ClfHead, self).__init__()
+        self.head = nn.Linear(d_model, n_output)
+
+    def forward(self, x):
+        """
+        x: tensor [batch_size x seq_len x d_model]
+        output: tensor [batch_size x num_classes]
+        """
+        # x = x[:, 0, :]     # Only use the [sos] token
+        x = x.mean(dim=1)    # Average pool over the hidden states
+        return self.head(x)
+=======
+import torch.nn as nn
+from layers.snippets import SigmoidRange
+
+class PretrainHead(nn.Module):
+    def __init__(self, d_model, n_output):
+        super(PretrainHead, self).__init__()
+        self.head = nn.Linear(d_model, n_output + 1)  # +1 to include the padding token as a class
+
+    def forward(self, x):
+        """
+        x: tensor [batch_size x seq_len x d_model]
+        output: tensor [batch_size x seq_len x n_output]
+        """
+        return self.head(x)
+
+
+class ClfHead(nn.Module):
+    def __init__(self, d_model, n_output):
+        super(ClfHead, self).__init__()
+        self.head = nn.Linear(d_model, n_output)
+
+    def forward(self, x):
+        """
+        x: tensor [batch_size x seq_len x d_model]
+        output: tensor [batch_size x num_classes]
+        """
+        # x = x[:, 0, :]     # Only use the [sos] token
+        x = x.mean(dim=1)    # Average pool over the hidden states
+        return self.head(x)
+>>>>>>> 79adf705c1bc4c71af40b3b22de696eaf1c9a4f4
